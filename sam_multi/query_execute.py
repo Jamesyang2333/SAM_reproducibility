9291b63fe157acef52d83ebc2af867162e682698
jamesyang2333 <jamesyang2333@gmail.com>
2023-01-13 09:25:57 +0000

    hide the database configuration in config file

--- sam_multi/query_execute.py
+++ sam_multi/query_execute.py
@@ -2,6 +2,26 @@ import psycopg2
 import numpy as np
 import argparse
 import time
+from configparser import ConfigParser
+
+def config(config_db):
+  section = 'postgresql'
+  config_file_path = config_db
+  if(len(config_file_path) > 0 and len(section) > 0):
+
+    config_parser = ConfigParser()
+    config_parser.read(config_file_path)
+  
+    if(config_parser.has_section(section)):
+      config_params = config_parser.items(section)
+      db_conn_dict = {}
+
+      for config_param in config_params:
+        key = config_param[0]
+        value = config_param[1]
+        db_conn_dict[key] = value
+
+      return db_conn_dict
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--queries',
@@ -16,12 +36,9 @@ parser.add_argument('--cards',
                     help='Test querycardinality file')
 args = parser.parse_args()
 
-conn = psycopg2.connect(
-    host="localhost",
-    database="db_gen_uaeq_400_test_100",
-    user="jingyi",
-    port="5444"
-)
+params = config("./config.ini")
+print(params)
+conn = psycopg2.connect(**params)
 
 cur = conn.cursor()
 
